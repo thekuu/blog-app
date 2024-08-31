caef39e089893ced54f669e1846e772ae110768c
thekuu <127185902+thekuu@users.noreply.github.com>
2024-08-31 16:18:05 +0300

    update the logo

--- api/db.js
+++ api/db.js
@@ -1,9 +1,11 @@
-import mysql from "mysql"
+import dotenv from 'dotenv';
+dotenv.config();
+
+import mysql from 'mysql';
 
 export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "ta34..AB",
-    database: "blog"
-
-})
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,8 +4,19 @@ import {Link} from "react-router-dom";
 const Footer = () => {
     return (
         <footer>
-            <img src={logo} alt=""/>
-            <span>Made with love and <b>React.js</b>.</span>
+            <div className="footer-above">
+                <img src={logo} alt=""/>
+                <span>Made with love and <b>React.js</b>.</span>
+            </div>
+            <hr/>
+            <div className="footer-bottom">
+                <p className="footer-bottom-left">© 2024 Zekaryas Geremew. All rights reserved.</p>
+                <div className="footer-bottom-right">
+                    <p>Term of Services</p>
+                    <p>Privacy Policy</p>
+                    <p>Connect with me</p>
+                </div>
+            </div>
         </footer>
     )
  }
